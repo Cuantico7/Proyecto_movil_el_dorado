8013846c7b5710dffad7a3aab87b0ec7f300dbc5
Judalpalau <judalpalau@gmail.com>
2022-10-28 13:28:36 -0500

    Loggin

--- el_dorado/lib/features/login_page.dart
+++ el_dorado/lib/features/login_page.dart
@@ -1,3 +1,6 @@
+//import 'dart:html';
+
+import 'package:el_dorado/features/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -17,29 +20,60 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: SafeArea(
-        child: Column(children: [
-          Expanded(child: Container()),
-          Center(
-            child: Image(
-              image: AssetImage('assets/logo.png'),
-              width: 120,
-            ),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Padding(
-              padding: EdgeInsets.all(30),
-              child: Text(
+        child: Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(children: [
+              Expanded(child: Container()),
+              Center(
+                child: Image(
+                  image: AssetImage('assets/logo.png'),
+                  width: 120,
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
                 "Un lugar del mundo en la palma de tu mano",
                 style: TextStyle(
                     fontSize: 36,
                     fontWeight: FontWeight.w300,
                     color: Colors.black.withOpacity(0.5)),
                 textAlign: TextAlign.center,
-              )),
-          Expanded(child: Container()),
-        ]),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Usuario",
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.person),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Contraseña",
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.vpn_key),
+                ),
+              ),
+              SizedBox(
+                height: 36,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SplashPage()));
+                },
+                child: Text("Ingresar"),
+              ),
+              Expanded(child: Container()),
+            ])),
       ),
     );
   }
